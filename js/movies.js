806e1f690356beeea2622fef4ab762b711276b3b
Towsif <towsif@live.com>
2022-01-22 05:07:09 +0800

    Full Website

---
+++ js/movies.js
@@ -0,0 +1,65 @@
+function getMovies() {
+    return [
+        {
+            id:1,
+            type:"now",
+            thumbnail:"casablanca.png",
+            src:"http://courses.cs.cityu.edu.hk/cs2204/video/casablanca-s.mp4",
+            name:"Casablanca",
+            cast:"Humphrey Bogart, Ingrid Bergmen",
+            director:"Michael Curtiz",
+            duration: 130
+        },
+        {
+            id:2,
+            type:"now",
+            thumbnail:"chocolate.png",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/cakemaking-s.mp4",
+            name:"Cake Attack",
+            cast:"Miles Tiller, John Jake",
+            director:"Elvis Presley",
+            duration: 100
+        },
+        {
+            id:3,
+            type:"now",
+            thumbnail:"wildlife.png",
+            src:"http://courses.cs.cityu.edu.hk/cs2204/video/wildlife.mp4",
+            name:"Big Buck",
+            cast:"Big Bunny, Little Fox",
+            director:"Forest King",
+            duration: 140
+        },
+        {
+            id:4,
+            type:"upcoming",
+            thumbnail:"nature.png",
+            src:"http://courses.cs.cityu.edu.hk/cs2204/video/nature.mp4",
+            name:"Wild Life",
+            cast:"Parrot, birds",
+            director:"Mother Nature",
+            duration: 130
+        },
+        {
+            id:5,
+            type:"upcoming",
+            thumbnail:"barbecue.png",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/barbecue.mp4",
+            name:"Fried Chicken",
+            cast:"Parrot, birds",
+            director:"Cody Banks",
+            duration: 110
+        },
+        {
+            id:6,
+            type:"upcoming",
+            thumbnail:"bigbuck.png",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/bigbuck-s.mp4",
+            name:"Nature",
+            cast:"Parrot, birds",
+            director:"Hello Lee",
+            duration: 120
+        }
+    ];
+}
+
